6e2e294e5ff4a9f8978e6470f8003e0592e958f2
Michael Kearns <mobiuscog@users.noreply.github.com>
2022-08-26 13:28:24 +0100

    Surface Normal shading - some kind of magic

--- src/ray.rs
+++ src/ray.rs
@@ -24,20 +24,26 @@ impl Ray {
     }
 
     pub fn colour(&self) -> Colour {
-        if self.hit_sphere(&Point3::new(0., 0., -1.), 0.5) {
-            return Colour::new(1., 0., 0.);
+        let mut t = self.hit_sphere(&Point3::new(0., 0., -1.), 0.5);
+        if t > 0. {
+            let n = (self.at(t) - Vector3::new(0., 0., -1.)).unit_vector();
+            return 0.5 * Colour::new(n.x() + 1., n.y() + 1., n.z() + 1.);
         }
         let unit_direction = self.direction().unit_vector();
-        let t = (unit_direction.y() + 1.) * 0.5;
+        t = 0.5 * (unit_direction.y() + 1.);
         (1. - t) * Colour::new(1., 1., 1.) + t * Colour::new(0.5, 0.7, 1.)
     }
 
-    fn hit_sphere(self, center: &Point3, radius: f64) -> bool {
+    fn hit_sphere(self, center: &Point3, radius: f64) -> f64 {
         let oc = self.origin() - center;
         let a = self.direction().dot(self.direction());
         let b = 2. * oc.dot(self.direction());
         let c = oc.dot(oc) - radius * radius;
         let discriminant = b * b - 4. * a * c;
-        discriminant > 0.
+        return if discriminant < 0. {
+            -1.0
+        } else {
+            (-b - discriminant.sqrt()) / (2.0 * a)
+        };
     }
 }
